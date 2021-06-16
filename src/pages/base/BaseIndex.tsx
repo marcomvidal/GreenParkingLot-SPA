7d799dc4cfc1d774ca3f48900941bd55f969c678
Marco M. Vidal <marcomvidal@gmail.com>
2021-06-15 23:21:46 -0300

    feat: react router addition

--- src/pages/base/BaseIndex.tsx
+++ src/pages/base/BaseIndex.tsx
@@ -1,11 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { NavigationBar } from "./NavigationBar";
+import { CarsIndex } from "../cars/CarsIndex";
+import { HomeIndex } from "../home/HomeIndex";
 
-export function BaseIndex() {
-  return (
-    <>
-      <NavigationBar />
-      <Container />
-    </>
-  );
-}
+export const BaseIndex = () => (
+  <>
+    <NavigationBar />
+    <Container>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <HomeIndex />
+          </Route>
+          <Route path='/cars'>
+            <CarsIndex />
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
+  </>
+);
+
---
+++ src/pages/base/links.ts
@@ -0,0 +1,9 @@
+export type Link = {
+  link: string,
+  label: string,
+};
+
+export const LINKS: Link[] = [
+  { label: 'Home', link: '/' },
+  { label: 'Cars', link: '/cars' },
+];
