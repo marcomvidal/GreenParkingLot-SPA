b73d4f84bcd933775ca1b81680c35e6935a171cf
Marco M. Vidal <marcomvidal@gmail.com>
2021-07-29 21:27:30 -0300

    fix: app as parent of the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import Base from "./pages/Base";
+import App from "App";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Base />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
