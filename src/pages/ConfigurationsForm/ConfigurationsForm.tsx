07984e0583db819c8e8e29a9130441786d6ffa7c
Marco M. Vidal <marcomvidal@gmail.com>
2021-10-07 22:23:11 -0300

    fix(configuration): form submitting working

--- src/pages/ConfigurationsForm/ConfigurationsForm.tsx
+++ src/pages/ConfigurationsForm/ConfigurationsForm.tsx
@@ -1,77 +1,93 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import FormField from "components/FormField";
-import SaveButton from "components/SaveButton";
-import Configuration from "models/Configuration";
-import { Card, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { RootState } from "store/types";
+import { Alert, Card, Form, FormControl, InputGroup } from "react-bootstrap";
 import { number, object, SchemaOf, string } from "yup";
+import Configuration from "models/Configuration";
+import FormField from "components/FormField";
+import SaveButton from "components/SaveButton";
+import CurrencyFormField from 'components/CurrencyFormField';
 import WhiteCardFooter from "./components/WhiteCardFooter";
+import { save } from "services/ConfigurationsService";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Configurations = () => {
   const user = useSelector((state: RootState) => state.user);
+  const [wasSuccessfullySubmited, setWasSuccessfullySubmited] = useState(false);
 
-  const onSubmit = (data: Configuration) => {
-    alert(data);
+  const onSubmit = (configuration: Configuration) => {
+    save(configuration);
+    setWasSuccessfullySubmited(true);
+    reset();
   };
   
   const schema: SchemaOf<Configuration> = object({
-    name: string().label('Name').required(),
+    name: string().label('Name').min(3).required(),
     email: string().label('E-mail').min(5).required(),
     password: string().label('Password').min(6).required(),
-    baseTax: number().label('Base tax').required(),
+    baseTax: number().label('Base tax').min(0).required().typeError('Base tax must be a number'),
   }).defined();
 
   const {
     handleSubmit,
     register,
-    formState: { errors, isValid },
+    reset,
+    formState: { errors, isValid, isDirty },
   } = useForm<Configuration>({
     defaultValues: { name: user.name, email: user.email, baseTax: user.baseTax, password: '' },
     resolver: yupResolver(schema),
     mode: 'onChange',
   });
 
+  useEffect(() => {
+    if (isDirty) setWasSuccessfullySubmited(false);
+  }, [isDirty]);
+
   return (
-    <Card>
-      <Card.Body>
-        <h3 className='mb-4'>Configurations</h3>
-        <Form onSubmit={handleSubmit(onSubmit)} method='POST'>
-          <FormField
-            id='name'
-            label='Name'
-            type='name'
-            errors={errors.name}
-            {...register('name')}
-          />
-          <FormField
-            id='email'
-            label='E-mail'
-            type='email'
-            errors={errors.email}
-            {...register('email')}
-          />
-          <FormField
-            id='password'
-            label='Password'
-            type='password'
-            errors={errors.password}
-            {...register('password')}
-          />
-          <FormField
-            id='baseTax'
-            label='Base tax'
-            type='number'
-            errors={errors.baseTax}
-            {...register('baseTax')}
-          />
-        </Form>
-      </Card.Body>
-      <WhiteCardFooter>
-        <SaveButton isDisabled={!isValid} className='float-right' />
-      </WhiteCardFooter>
-    </Card>
+    <Form onSubmit={handleSubmit(onSubmit)} method='POST'>
+      <Card>
+        <Card.Body>
+          <h3 className='mb-4'>Configurations</h3>
+            {wasSuccessfullySubmited && 
+              <Alert variant='success'>The configurations were successfully updated.</Alert>
+            }
+            <FormField
+              id='name'
+              label='Name'
+              type='name'
+              errors={errors.name}
+              {...register('name')}
+            />
+            <FormField
+              id='email'
+              label='E-mail'
+              type='email'
+              errors={errors.email}
+              {...register('email')}
+            />
+            <FormField
+              id='password'
+              label='Password'
+              type='password'
+              errors={errors.password}
+              {...register('password')}
+            />
+            <CurrencyFormField
+              id='baseTax'
+              label='Base tax'
+              type='number'
+              currency='R$'
+              errors={errors.baseTax}
+              {...register('baseTax')}
+            />
+        </Card.Body>
+        <WhiteCardFooter>
+          <SaveButton isDisabled={!isValid} className='float-right' />
+        </WhiteCardFooter>
+      </Card>
+    </Form>
   );
 };
 
---
+++ src/services/ConfigurationsService.ts
@@ -0,0 +1,5 @@
+import Configuration from "models/Configuration";
+
+const save = (configuration: Configuration) => {};
+
+export { save };
